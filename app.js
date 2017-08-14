c7778647dc1401a4e69ca5700d26f9e982e14ad1
Marco Zangari <mjzangari@gmail.com>
2017-08-14 15:01:55 -0700

    added random number generator to store locations; updated index.html to reflect shop name; updated sales.html with h1 for daily projected sales

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ maxNumCust: 65,
 avgNumCookiesCust: 6.3,
 
 };
+1stPike.Math.floor(Math.random() * (65 - 23) + 23);
+
 var
 
 var SeaTac = {
@@ -16,6 +18,7 @@ maxNumCust: 24,
 avgNumCookiesCust: 1.2,
 
 };
+SeaTac.Math.floor(Math.random() * (24 - 3) + 3);
 
 
 var SeattleCenter = {
@@ -25,6 +28,8 @@ maxNumCust: 38,
 avgNumCookiesCust: 3.7
 
 };
+SeattleCenter.Math.floor(Math.random() * (38 - 11) + 11);
+
 
 var CapHill = {
 name: 'Capitol Hill',
@@ -33,6 +38,8 @@ maxNumCust: 38,
 avgNumCookiesCust: 2.3,
 
 };
+CapHill.Math.floor(Math.random() * (38 - 20) + 20);
+
 
 var Alki = {
 name: 'Alki',
@@ -41,3 +48,4 @@ maxNumCust: 16,
 avgNumCookiesCust: 4.6,
 
 };
+Alki.Math.floor(Math.random() * (16 - 2) + 2);
