66844d39d1b880ea18450fac1eab358724392a74
Marco Zangari <mjzangari@gmail.com>
2017-08-14 14:36:25 -0700

    added values, minNumCust, maxNumCust, avgNumCookiesCust, and name to each of the four objects

---
+++ app.js
@@ -0,0 +1,43 @@
+'user strict';
+
+var 1stPike = {
+name: '1st and Pike',
+minNumCust: 23,
+maxNumCust: 65,
+avgNumCookiesCust: 6.3,
+
+};
+var
+
+var SeaTac = {
+name: 'SeaTac Airport',
+minNumCust: 3,
+maxNumCust: 24,
+avgNumCookiesCust: 1.2,
+
+};
+
+
+var SeattleCenter = {
+name: 'Seattle Center',
+minNumCust: 11,
+maxNumCust: 38,
+avgNumCookiesCust: 3.7
+
+};
+
+var CapHill = {
+name: 'Capitol Hill',
+minNumCust: 20,
+maxNumCust: 38,
+avgNumCookiesCust: 2.3,
+
+};
+
+var Alki = {
+name: 'Alki',
+minNumCust: 2,
+maxNumCust: 16,
+avgNumCookiesCust: 4.6,
+
+};
