9f5d1ed17bcab03f9a4ba7edfe46561316cd6bca
Marco Zangari <mjzangari@gmail.com>
2017-08-14 19:06:27 -0700

    debugged first store's methods

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ minNumCust: 23,
 maxNumCust: 65,
 avgNumCookiesCust: 6.3,
 avgSimCookies: [],
-
+hoursOp: ['6 AM: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ']
   firstPikeRandCust: function() {
   return Math.floor(Math.random() * (this.maxNumCust - this.minNumCust) + this.minNumCust);
   },
