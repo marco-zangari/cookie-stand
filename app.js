d7e8daefea680d8b3bc0b5f961ff2ea810708db3
Marco Zangari <mjzangari@gmail.com>
2017-08-15 16:15:43 -0700

    passed new values into constructor function, worked out bus in methods within object

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 'use strict';
 
-var hours = ['6 AM: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+var hours = ['6 AM: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
 
-function CookieStore(name, minCust, maxCust, avgCookies, ) {
-  this.name = '1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki';
-  this.minCust = 23, 3, 11, 20, 2;
-  this.maxCust = 65, 24, 38, 38, 16;
-  this.avgCookies = 6.3, 1.2, 3.7, 2.3, 4.6;
+function CookieStore(name, minCust, maxCust, avgCookies) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookies = avgCookies;
   this.avgSimCookies = [];
 
   this.randCust = function() {
-    return Math.floor(Math.random() * (this.maxNumCust - this.minNumCust) + this.minNumCust);
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     };
   this.eachHrSales = function() {
     for (var i = 0; i < 14; i++) {
       var hrSales = Math.ceil(this.randCust() * this.avgCookies);
-      this.avgSimCookies.push(Math.ceil(hrSales);
+      this.avgSimCookies.push(Math.ceil(hrSales));
       }
     };
   this.salesReport = function () {
     for (var i = 0; i < hours.length; i++) {
     var randomSales = this.eachHrSales(i);
-    this.avgSimCookies.push(this.hoursOp[i] + pikeRandomSales + ' cookies');
+    this.avgSimCookies.push(hours[i] + randomSales + ' cookies');
       }
     };
-CookieStore.name
-//call the functions!!!
+}
 
-    var listItems = document.create.Element('li');
+var pike = new CookieStore ('1st and Pike', 23, 65, 6.3);
+var seatac = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
+var seattlectr = new CookieStore ('Seattle Center', 11, 38, 3.7);
+var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
+var alki = new CookieStore ('Alki', 2, 16, 4.6);
+
+//pike.salesReport();
+
+var listItems = document.create.Element('li');
     listItems.innerText = "Total: " + this.total + 'cookies';
     list.appendChild(listItems);
-
-//    this.total+=pikeRandomSales;
-    }
-  },
-};
-
-firstPike.salesReport();
-
+    
 var salesReport = document.createElement('li');
 firstPikeSalesReport.innerText = this.hoursOp[i] + this.avgSimCookies[i] + '.'; // <--- if you want to change the inner text
 firstPikeSalesReport.appendChild(newLi);*/
