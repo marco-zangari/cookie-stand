841099431a648a2f38ddcc43e8f9c9ec7b31efba
Marco Zangari <mjzangari@gmail.com>
2017-08-15 18:28:08 -0700

    worked through methods in function

--- app.js
+++ app.js
@@ -13,36 +13,31 @@ function CookieStore(name, minCust, maxCust, avgCookies) {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     };
   this.eachHrSales = function() {
-    for (var i = 0; i < 14; i++) {
+    this.avgSimCookies = [];
+    for (var i = 0; i < hours.length; i++) {
       var hrSales = Math.ceil(this.randCust() * this.avgCookies);
-      this.avgSimCookies.push(Math.ceil(hrSales));
+      this.avgSimCookies.push(hrSales);
       }
     };
+
   this.salesReport = function () {
     for (var i = 0; i < hours.length; i++) {
-    var randomSales = this.eachHrSales(i);
-    this.avgSimCookies.push(hours[i] + randomSales + ' cookies');
-      }
+      var
+      this.avgSimCookies.push(hours[i] + ' cookies');
+      this.eachHrSales();
     };
+  }
 }
-
 var pike = new CookieStore ('1st and Pike', 23, 65, 6.3);
 var seatac = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
 var seattlectr = new CookieStore ('Seattle Center', 11, 38, 3.7);
 var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
 var alki = new CookieStore ('Alki', 2, 16, 4.6);
 
-//pike.salesReport();
 
-var listItems = document.create.Element('li');
-    listItems.innerText = "Total: " + this.total + 'cookies';
-    list.appendChild(listItems);
-    
-var salesReport = document.createElement('li');
-firstPikeSalesReport.innerText = this.hoursOp[i] + this.avgSimCookies[i] + '.'; // <--- if you want to change the inner text
-firstPikeSalesReport.appendChild(newLi);*/
-console.log(firstPike.total);
+var salesList = document.getElementById('list');
 
-var listFirstPike = document.getElementById('firstPikeSalesReport');
-var listFirstPikeMerge = document.createElement('li');
-firstPikeSalesReport.appendChild(listFirstPikeMerge);
+for (var i = 0; i < hours.length; i++);
+var listItems = document.createElement('li');
+listItems.innerText = hours[i] + pike.salesReport() + 'cookies.'
+salesList.appendChild(listItems);
