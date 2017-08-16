ae5eedc695817da0f60df5e5c60a015d626160cb
Marco Zangari <mjzangari@gmail.com>
2017-08-15 19:22:15 -0700

    got the function to make a list. That is an accomplishment.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6 AM: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+var hours = ['6 am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
 
 function CookieStore(name, minCust, maxCust, avgCookies) {
   this.name = name;
@@ -19,25 +19,21 @@ function CookieStore(name, minCust, maxCust, avgCookies) {
       this.avgSimCookies.push(hrSales);
       }
     };
-
   this.salesReport = function () {
+    this.eachHrSales();
     for (var i = 0; i < hours.length; i++) {
-      var
-      this.avgSimCookies.push(hours[i] + ' cookies');
-      this.eachHrSales();
+      var salesList = document.getElementById('list');
+      var listItems = document.createElement('li');
+      listItems.innerText = hours[i] + this.avgSimCookies[i] + ' cookies.';
+      salesList.appendChild(listItems);
     };
   }
 }
+
 var pike = new CookieStore ('1st and Pike', 23, 65, 6.3);
 var seatac = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
 var seattlectr = new CookieStore ('Seattle Center', 11, 38, 3.7);
 var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
 var alki = new CookieStore ('Alki', 2, 16, 4.6);
 
-
-var salesList = document.getElementById('list');
-
-for (var i = 0; i < hours.length; i++);
-var listItems = document.createElement('li');
-listItems.innerText = hours[i] + pike.salesReport() + 'cookies.'
-salesList.appendChild(listItems);
+pike.salesReport();
