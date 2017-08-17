ca501bf81152171f0c1229912c8c089dc2a93d79
Marco Zangari <mjzangari@gmail.com>
2017-08-16 20:30:36 -0700

    got the table to populate with real numbers

--- app.js
+++ app.js
@@ -19,48 +19,35 @@ function CookieStore(name, minCust, maxCust, avgCookies) {
       this.avgSimCookies.push(hrSales);
       }
     };
-// this.salesReport = function () {
-//     for (var i = 0; i < hours.length; i++) {
-//       var salesList = document.getElementById('list');
-//       var listItems = document.createElement('li');
-//       listItems.innerText = hours[i] + this.avgSimCookies[i];
-//       salesList.appendChild(listItems);
-//       return listItems.innerText;
-//       }
-//     };
+
 
   this.cookieTable = function () {
-    var storesArray = ['pike', 'seatac', 'seattlectr', 'capitol', 'alki'];
-    for (var i = 0; i < storesArray.length; i++) {
+    //var storesArray = ['pike', 'seatac', 'seattlectr', 'capitol', 'alki'];
+    //for (var i = 0; i < storesArray.length; i++) {
       var table = document.getElementById('salesTable');
       var firstRow = document.createElement('tr');
       table.appendChild(firstRow);
 
-      var salesData = document.createElement('td');
-      salesData.innerText = storesArray[i];
-      firstRow.appendChild(salesData);
+      var storeData = document.createElement('td');
+      storeData.innerText = this.name;
+      firstRow.appendChild(storeData);
 
+      this.avgSimCookies = [];
+      this.eachHrSales();
+      for (var j = 0; j < this.avgSimCookies.length; j++) {
+        var salesData = document.createElement('td');
+        salesData.innerText = this.avgSimCookies[j];
+        firstRow.appendChild(salesData);
 
-      // var row = document.createElement('tr');
-      // tr.innerText.appendChild(storesArray);
-
-
-      // for (var j = 0; j < hours.length; j++) {
-      //   var td = document.createElement('td');
-      //   td.innerText = this.avgSimCookies[j];
-      //   tableRow.appendChild(td);
-      //   row.appendChild(td);
-      //   }
-      }
+    //  }
     };
   }
-
-
+}
 var pike = new CookieStore ('1st and Pike', 23, 65, 6.3);
-// var seatac = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
-// var seattlectr = new CookieStore ('Seattle Center', 11, 38, 3.7);
-// var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
-// var alki = new CookieStore ('Alki', 2, 16, 4.6);
+var seatac = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
+var seattlectr = new CookieStore ('Seattle Center', 11, 38, 3.7);
+var capitol = new CookieStore ('Capitol Hill', 20, 38, 2.3);
+var alki = new CookieStore ('Alki', 2, 16, 4.6);
 
 //pike.salesReport();
 //seatac.salesReport();
@@ -69,3 +56,17 @@ var pike = new CookieStore ('1st and Pike', 23, 65, 6.3);
 //alki.salesReport();
 
 pike.cookieTable();
+seatac.cookieTable();
+seattlectr.cookieTable();
+capitol.cookieTable();
+alki.cookieTable();
+
+// this.salesReport = function () {
+//     for (var i = 0; i < hours.length; i++) {
+//       var salesList = document.getElementById('list');
+//       var listItems = document.createElement('li');
+//       listItems.innerText = hours[i] + this.avgSimCookies[i];
+//       salesList.appendChild(listItems);
+//       return listItems.innerText;
+//       }
+//     }
