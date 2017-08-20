4406f0305f16ffc683d3b89ca842e42d949e8a22
Marco Zangari <mjzangari@gmail.com>
2017-08-20 10:48:04 -0700

    got the function for the first store to display properly. Need to produce the other stores' data too.

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
 'use strict';
 
 var firstPike = {
-name: '1st and Pike',
-minNumCust: 23,
-maxNumCust: 65,
-avgNumCookiesCust: 6.3,
-avgSimCookies: [],
-hoursOp: ['6 AM: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
-total: 0,
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesCust: 6.3,
+  avgSimCookies: [],
+  hoursOp: ['6 am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+  total: 0,
 
   randCust: function() {
-  return Math.floor(Math.random() * (this.maxNumCust - this.minNumCust) + this.minNumCust);
+  return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   },
-
   eachHrSales: function () {
-  for (var i = 0; i < 2; i++) {
-    var hrSales = Math.ceil(this.randCust() * this.avgNumCookiesCust);
+    this.avgSimCookies = [];
+    this.total = 0;
+  for (var i = 0; i < this.hoursOp.length; i++) {
+    var hrSales = Math.ceil(this.randCust() * this.avgCookiesCust);
     this.avgSimCookies.push(hrSales);
-    console.log(hrSales);
+    this.total += hrSales;
     }
   },
-
   salesReport: function () {
-    for (var i = 0; i < this.hoursOp.length; i++) {
-    var pikeRandomSales = this.eachHrSales();
-    this.avgSimCookies.push(this.hoursOp[i] + pikeRandomSales + ' cookies');
-    console.log(this.eachHrSales());
-
-//    this.total+=pikeRandomSales;
-    }
-  },
+   this.eachHrSales();
+   for (var i = 0; i < this.hoursOp.length; i++) {
+   var sales = document.getElementById('salesPike');
+   var storeSales = document.createElement('ul');
+   storeSales.innerText = this.hoursOp[i] + this.avgSimCookies[i] + ' cookies';
+   sales.appendChild(storeSales);
+      }
+    var dailyTotal = document.createElement('ul')
+    dailyTotal.innerText = 'Total: ' + this.total + ' cookies';
+    sales.appendChild(dailyTotal);
+    },
 };
 
 firstPike.salesReport();
-//firstPike.hoursOp[];
-
-/*var newLi = document.createElement('li');
-newLi.innerText = this.hoursOp[i] + this.avgSimCookies[i] + '.'; // <--- if you want to change the inner text
-firstPikeSalesReport.appendChild(newLi);*/
-console.log(firstPike.total);
-
-/*var listFirstPike = document.getElementById('firstPikeSalesReport');
-var listFirstPikeMerge = document.createElement('li');
-firstPike.firstPikeSalesReport.appendChild(listFirstPikeMerge);
-
-
-}*/
 
 /*
 var SeaTac = {
