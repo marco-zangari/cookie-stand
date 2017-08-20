aac254e893ca0680528f36272414660981fea631
Marco Zangari <mjzangari@gmail.com>
2017-08-20 14:09:19 -0700

    got the seattle center list to work

--- app.js
+++ app.js
@@ -71,22 +71,49 @@ salesReport: function () {
   sales.appendChild(dailyTotal);
   },
 };
-
 seaTac.salesReport();
 
-var SeattleCenter = {
-name: 'Seattle Center',
-minNumCust: 11,
-maxNumCust: 38,
-avgNumCookiesCust: 3.7
+var seattleCenter = {
+  name: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesCust: 3.7,
+  avgSimCookies: [],
+  hoursOp: ['6 am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+  total: 0,
 
+randCust: function () {
+  return Math.floor(Math.random() * ((this.maxCust - this.minCust) + this.maxCust));
+  },
+eachHrSales: function () {
+  this.avgSimCookies = [];
+  this.total = 0;
+  for (var i = 0; i < this.hoursOp.length; i++) {
+  var hrSales = Math.floor(this.randCust() * this.avgCookiesCust);
+  this.avgSimCookies.push(hrSales);
+  this.total += hrSales;
+    }
+  },
+salesReport: function () {
+  this.eachHrSales();
+  for (var i = 0; i < this.hoursOp.length; i++) {
+  var sales = document.getElementById('salesSeattleCenter');
+  var storeSales = document.createElement('ul');
+  storeSales.innerText = this.hoursOp[i] + this.avgSimCookies[i] + ' cookies';
+  sales.appendChild(storeSales);
+
+  var storeTotal = document.createElement('ul');
+  storeTotal.innerText = 'Total: ' + this.total + ' cookies';
+    }
+  },
 };
+seattleCenter.salesReport();
 
 var CapHill = {
 name: 'Capitol Hill',
-minNumCust: 20,
-maxNumCust: 38,
-avgNumCookiesCust: 2.3,
+minCust: 20,
+maxCust: 38,
+avgCookiesCust: 2.3,
 
 };
 
