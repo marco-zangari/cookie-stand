c99464d6101d65f999a27e7af231385bae3a9fdc
Marco Zangari <mjzangari@gmail.com>
2017-08-20 14:52:41 -0700

    finished Capitol Hill function, made it work

--- app.js
+++ app.js
@@ -101,25 +101,59 @@ salesReport: function () {
   var storeSales = document.createElement('ul');
   storeSales.innerText = this.hoursOp[i] + this.avgSimCookies[i] + ' cookies';
   sales.appendChild(storeSales);
-
-  var storeTotal = document.createElement('ul');
-  storeTotal.innerText = 'Total: ' + this.total + ' cookies';
     }
+    var dailyTotal = document.createElement('ul');
+    dailyTotal.innerText = 'Total: ' + this.total + ' cookies';
+    sales.appendChild(dailyTotal);
   },
 };
 seattleCenter.salesReport();
 
-var CapHill = {
+var capHill = {
 name: 'Capitol Hill',
 minCust: 20,
 maxCust: 38,
 avgCookiesCust: 2.3,
+avgSimCookies: [],
+hoursOp: ['6 am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+total: 0,
 
+randCust: function () {
+  return Math.floor(Math.random() * ((this.maxCust - this.minCust) + this.minCust));
+  },
+eachHrSales: function () {
+  this.avgSimCookies = [];
+  this.total = 0;
+  for (var i = 0; i < this.hoursOp.length; i++) {
+    var hrSales = Math.floor(this.randCust() * this.avgCookiesCust);
+    this.avgSimCookies.push(hrSales);
+    this.total += hrSales;
+    }
+  },
+salesReport: function () {
+  this.eachHrSales();
+  for (var i = 0; i < this.hoursOp.length; i++) {
+    var sales = document.getElementById('salesCapHill');
+    var salesReport = document.createElement('ul');
+    salesReport.innerText = this.hoursOp[i] + this.avgSimCookies[i] + ' cookies';
+    sales.appendChild(salesReport);
+    }
+    var dailyTotal = document.createElement('ul');
+    dailyTotal.innerText = 'Total: ' + this.total + ' cookies';
+    sales.appendChild(dailyTotal);
+  },
 };
+capHill.salesReport();
 
 var Alki = {
-name: 'Alki',
-minNumCust: 2,
-maxNumCust: 16,
-avgNumCookiesCust: 4.6,
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookiesCust: 4.6,
+  avgSimCookies: [],
+  hoursOp: ['6 am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
+  total: 0,
+
+
+
 };
